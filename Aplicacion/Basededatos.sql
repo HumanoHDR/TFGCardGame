1170b200aed4977ee36d724ce5177093340fb046
Humano <87076583+HumanoHDR@users.noreply.github.com>
2024-04-10 12:05:17 +0200

    Base de datos re hecha

--- Aplicacion/Basededatos.sql
+++ Aplicacion/Basededatos.sql
@@ -16,7 +16,6 @@ CREATE TABLE IF NOT EXISTS Card (
     effect VARCHAR(500)
 );
 
--- Crear la tabla de Mazos
 CREATE TABLE IF NOT EXISTS Deck (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -24,7 +23,6 @@ CREATE TABLE IF NOT EXISTS Deck (
     FOREIGN KEY (user_id) REFERENCES User(id)
 );
 
--- La tabla de Cartas_Mazos para manejar la relación muchos a muchos entre Cartas y Mazos
 CREATE TABLE IF NOT EXISTS Deck_Card (
     deck_id INT,
     card_id INT,
@@ -33,26 +31,16 @@ CREATE TABLE IF NOT EXISTS Deck_Card (
     FOREIGN KEY (card_id) REFERENCES Card(id)
 );
 
--- Crear la tabla de Juegos
-CREATE TABLE IF NOT EXISTS Game (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    result VARCHAR(255),
-    move_reg TEXT,
-    user_id INT,
-    FOREIGN KEY (user_id) REFERENCES User(id)
+CREATE TABLE IF NOT EXISTS `Game` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `Result` VARCHAR(255) NOT NULL,
+    `MoveReg` TEXT,
+    `Date` DATETIME NOT NULL,
+    `User_id` INT,
+    `Favorite` BOOLEAN,
+    FOREIGN KEY (`User_id`) REFERENCES `User`(`id`)
 );
 
--- Crear la tabla de Juegos Guardados
-CREATE TABLE IF NOT EXISTS SavedGame (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    game_id INT,
-    user_id INT,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (game_id) REFERENCES Game(id),
-    FOREIGN KEY (user_id) REFERENCES User(id)
-);
-
--- La tabla de Amistades para manejar la relación muchos a muchos entre Usuarios
 CREATE TABLE IF NOT EXISTS Friendship (
     user_id1 INT,
     user_id2 INT,
